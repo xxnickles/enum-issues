6aabca2fef2d1b8ce40f9ea15232a117b3c631f8
Nickle <xxnickles@gmail.com>
2024-12-07 01:45:45 -0500

    Initial comit

---
+++ EnumInCollections/Components/Pages/Types.cs
@@ -0,0 +1,35 @@
+﻿namespace EnumInCollections.Components.Pages;
+
+public enum Test
+{
+    A,
+    B
+}
+
+public class BadComplexProperty
+{
+    public string? StringValue { get; set; }
+    public Test? EnumValue { get; set; }
+}
+
+public class BadBiding
+{
+    public Test? EnumValueParent { get; set; }
+    public BadComplexProperty[] Items { get; set; } = [new(), new() { EnumValue = Test.A, StringValue = "Nothing" }];
+}
+
+
+public class GoodComplexProperty
+{
+    public string? StringValue { get; set; }
+    public string? EnumValue { get; set; }
+}
+
+public class GoodBiding
+{
+    public Test? EnumValueParent { get; set; }
+    public GoodComplexProperty[] Items { get; set; } = [new(), new() { EnumValue = Test.A.ToString(), StringValue = "Nothing" }];
+}
+
+
+
